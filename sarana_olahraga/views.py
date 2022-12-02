063c2e26a4f1d1b8489a98e9a2c44387697e6b36
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2022-12-02 20:58:30 +0700

    Update implementation getSaranaOlaharaga

--- sarana_olahraga/views.py
+++ sarana_olahraga/views.py
@@ -8,6 +8,5 @@ def getSaranaOlaharaga(request, ID_gor):
     if request.method == "GET":
         context = {}
         gor, created = GOR.objects.get_or_create(ID_gor=ID_gor)
-        print(gor.nama)
         context = {"gor": gor, "daftar_sarana": gor.getSaranaGor()}
         return render(request, "sarana_olahraga.html", context)
