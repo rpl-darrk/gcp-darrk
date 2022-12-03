b527a6c00a0a85fa2f2149586bcab6d2d923a5f3
KennJthn <kennjthn12@gmail.com>
2022-12-03 13:24:23 +0700

    Add authentication before making a reservation

--- jadwal_reservasi/views.py
+++ jadwal_reservasi/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from jadwal_reservasi.models import Jadwal_Reservasi, Status_Reservasi
 
 def reservasi(request, id_jadwal):
-    jadwal_reservasi = Jadwal_Reservasi.objects.get(id_jadwal=id_jadwal)
+    if request.user.is_authenticated:
+        jadwal_reservasi = Jadwal_Reservasi.objects.get(id_jadwal=id_jadwal)
 
-    if (jadwal_reservasi.status.__eq__(Status_Reservasi.VACANT)):
-        jadwal_reservasi.status = Status_Reservasi.ORDERED
-        jadwal_reservasi.save()
-        return render(request, "reservasi_berhasil.html")
+        if (jadwal_reservasi.status.__eq__(Status_Reservasi.VACANT)):
+            jadwal_reservasi.status = Status_Reservasi.ORDERED
+            jadwal_reservasi.save()
+            return render(request, "reservasi_berhasil.html")
+        else:
+            return render(request, "reservasi_gagal.html")
     else:
-        return render(request, "reservasi_gagal.html")
+        return redirect("login")
