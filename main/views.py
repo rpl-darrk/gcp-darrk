4de39420d1a44be01f458a9ceb77a0b4e2e99795
KennJthn <kennjthn12@gmail.com>
2022-12-09 16:44:25 +0700

    Migrate gor list from homepage to a new endpoint

--- main/views.py
+++ main/views.py
@@ -4,11 +4,14 @@ from sarana_olahraga.models import GOR
 
 
 def home(request):
+    return render(request, "main/home.html")
+
+def gor_list(request):
     if request.user.is_authenticated:
         gor_list = GOR.objects.all()
         context = {
             "gor_list": gor_list,
         }
-        return render(request, "main/home.html", context)
+        return render(request, "main/gor_list.html", context)
     else:
         return redirect("login")
