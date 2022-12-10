d2377913f9ff91609680d83a069f61241133ca57
hollowsyde <alfatih.aditya@ui.ac.id>
2022-12-10 12:51:25 +0700

    Melengkapi fitur mengatur_jadwal_reservasi

--- sarana_olahraga/url/urls_mengelola_sarana.py
+++ sarana_olahraga/url/urls_mengelola_sarana.py
@@ -4,10 +4,16 @@ from ..view import views_mengelola_sarana
 app_name = "mengelola_sarana_olahraga"
 
 urlpatterns = [
-    path("", views_mengelola_sarana.index, name="index"),
-    path("post_sarana/", views_mengelola_sarana.post_sarana, name="post_sarana"),
+    path("",
+        views_mengelola_sarana.index, name="index"),
+    path("post_sarana/",
+        views_mengelola_sarana.post_sarana, name="post_sarana"),
     path("update_sarana/<str:id_sarana>",
          views_mengelola_sarana.update_sarana, name="update_sarana"),
     path("delete_sarana/<str:id_sarana>",
          views_mengelola_sarana.delete_sarana, name="delete_sarana"),
+    path("atur-jadwal/<str:id_sarana>",
+         views_mengelola_sarana.aturTabelJadwal, name="atur_tabel_jadwal"),
+    path("update-jadwal/<str:id_sarana>",
+         views_mengelola_sarana.updateTabelJadwal, name="update_tabel_jadwal"),
 ]
--- sarana_olahraga/view/views.py
+++ sarana_olahraga/view/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 import json
 
 from ..models import GOR, Jadwal_Reservasi, Sarana
-from pengguna.models import Pengurus_GOR
 
 
 def initTabelJadwal():
@@ -25,37 +24,6 @@ def initTabelJadwal():
     new_jadwal.save()
 
     return new_jadwal
-
-
-def updateTabelJadwal(request, id_sarana):
-
-    try:
-        Pengurus_GOR.objects.get(user=request.user)
-    except:
-        return HttpResponseForbidden()
-
-    if request.method == 'POST':
-
-        hari_buka_raw = request.POST.get('hari_buka')
-        jam_buka_raw = request.POST.get('jam_buka')
-
-        hari_buka = json.loads(hari_buka_raw)
-        jam_buka = json.loads(jam_buka_raw)
-        status_book = [[False for i in range(
-            len(hari_buka))] for i in range(len(jam_buka))]
-
-        jadwal = Sarana.objects.get(ID_sarana=id_sarana).id_jadwal
-
-        new_jadwal = Jadwal_Reservasi(
-            ID_jadwal=jadwal.ID_jadwal,
-            hari_buka=JSONEncoder().encode(hari_buka),
-            jam_buka=JSONEncoder().encode(jam_buka),
-            status_book=JSONEncoder().encode(status_book)
-        )
-
-        new_jadwal.save()
-
-    return render(request, 'trial.html')
 
 
 def showTabelJadwal(request, id_sarana):
