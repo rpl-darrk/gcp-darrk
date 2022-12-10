0bea333acd701647bbb11a87b406bcab535c293e
KennJthn <kennjthn12@gmail.com>
2022-12-10 11:30:50 +0700

    Remove reservasi from sarana_olahraga package

--- sarana_olahraga/url/urls_jadwal_reservasi.py
+++ sarana_olahraga/url/urls_jadwal_reservasi.py
@@ -4,8 +4,6 @@ from ..view import views_jadwal_reservasi
 app_name = "jadwal_reservasi"
 
 urlpatterns = [
-    path("reservasi/<id_jadwal>/",
-         views_jadwal_reservasi.reservasi, name="reservasi"),
     path("melihat_jadwal_reservasi/<str:id_gor>/<str:id_sarana>/",
          views_jadwal_reservasi.get_jadwal_reservasi, name="get_jadwal_reservasi"),
 ]
