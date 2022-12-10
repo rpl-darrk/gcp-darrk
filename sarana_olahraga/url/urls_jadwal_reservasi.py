54d4e796f749adfb116acc029f0276683089ab58
Dimas Ichsanul Arifin <dimas.ichsanul@ui.ac.id>
2022-12-10 12:34:22 +0700

    Implement Front End for Mengelola Sarana Olahraga and Melihat Jadwal

--- sarana_olahraga/url/urls_jadwal_reservasi.py
+++ sarana_olahraga/url/urls_jadwal_reservasi.py
@@ -6,6 +6,6 @@ app_name = "jadwal_reservasi"
 urlpatterns = [
     path("reservasi/<id_jadwal>/",
          views_jadwal_reservasi.reservasi, name="reservasi"),
-    path("melihat_jadwal_reservasi/<str:id_gor>/<str:id_sarana>/",
+    path("jadwal/<str:id_gor>/<str:id_sarana>/",
          views_jadwal_reservasi.get_jadwal_reservasi, name="get_jadwal_reservasi"),
 ]
