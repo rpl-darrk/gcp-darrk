b700e2a7cec19af0510a01c627a8c367e96de79b
KennJthn <kennjthn12@gmail.com>
2022-12-10 12:36:15 +0700

    Migrate reservasi feature to reservasi_sarana package

--- reservasi_sarana/tests.py
+++ reservasi_sarana/tests.py
@@ -29,34 +29,18 @@ class ReservasiSaranaTest(TestCase):
                                       url_foto="url_foto", alamat="alamat", nomor_telepon="nomor_telepon", pengurus=self.pengurus)
 
         self.jadwal_reservasi = Jadwal_Reservasi.objects.create(
-            hari_buka="[]", jam_buka="[[]]", status_book="[[]]")
+            ID_jadwal='1',
+            hari_buka=[True, True, True, True, True, False, False],
+            jam_buka=[['10.00', '11.00'], ['11.00', '12.00']],
+            status_book=[[True, True, True, True, True, True, True],
+                         [True, True, True, True, True, True, True]]
+        )
 
         self.sarana = Sarana.objects.create(ID_sarana="1",  nama="nama",
                                             url_foto="url_foto", jenis="jenis", deskripsi="deskripsi", gor=self.gor, id_jadwal_reservasi=self.jadwal_reservasi)
 
         self.sewa_sarana = Sewa_Sarana.objects.create(
             ID_sewa="1", biaya=120000.00, sarana=self.sarana, konsumen=self.konsumen, pengurus=self.pengurus, jam_booking=["10.00-11.00", 0])
-
-        self.user_konsumen2 = User.objects.create_user(
-            email="konsumen2@email.com",
-            username="konsumen2",
-            password="konsumen2",
-        )
-        self.konsumen2 = Konsumen_GOR.objects.create(
-            user=self.user_konsumen2,
-            nama="konsumen2",
-            nomor_telepon="081292929294",
-            status_loyalty="Basic Member"
-        )
-
-        self.sewa_sarana2 = Sewa_Sarana.objects.create(
-            ID_sewa="2",
-            biaya=120000.00,
-            sarana=self.sarana,
-            konsumen=self.konsumen2,
-            pengurus=self.pengurus,
-            jam_booking=["10.00-11.00", 0]
-        )
 
     def test_verifikasiPembayaran(self):
         self.detail = Detail_Pembayaran.objects.create(
@@ -132,3 +116,20 @@ class ReservasiSaranaTest(TestCase):
         self.client.login(username="pengurus", password="pengurus")
         response = self.client.get("/daftar-reservasi")
         self.assertEqual(response.status_code, 200)
+
+    def test_not_authenticated_response(self):
+        response = self.client.post(
+            "/reservasi/%s/%s/%s" % ("1", "1", "1|10.00|11.00"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_authenticated_response(self):
+        self.client.login(username="konsumen", password="konsumen")
+        response = self.client.post(
+            "/reservasi/%s/%s/%s" % ("1", "1", "1|10.00|11.00"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_method_response(self):
+        self.client.login(username="konsumen", password="konsumen")
+        response = self.client.get(
+            "/reservasi/%s/%s/%s" % ("1", "1", "1|10.00|11.00"))
+        self.assertEqual(response.status_code, 404)
